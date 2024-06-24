9714688cdf6348d1fef68646dc1d4dd9a46ef86a
SkyLiveCode <pongsaguy_2540@hotmail.com>
2024-06-24 09:38:49 +0700

    Update connect.php

--- html/system_crud/connect.php
+++ html/system_crud/connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "141515";
-$dbname = "database_ap";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
